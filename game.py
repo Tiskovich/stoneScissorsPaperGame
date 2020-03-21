0e8b1af13e732ba0d6c181f3cddf7410612a7eb9
Tiskovich <tiskovich.v.u@gmail.com>
2020-03-21 17:24:30 +0300

    create prototype for backend

--- game.py
+++ game.py
@@ -1,15 +1,55 @@
 import random
 import string
 
+STONE = 'stone'
+PAPER = 'paper'
+SCISSORS = 'scissors'
 
-class Info(object):
-    def __init__(self, team_size=2):
+
+class Game(object):
+    def __init__(self, team_size=2, player=None):
         self.team_size = team_size
         self.game_id = self.generate_room_id()
-        self.players = []
+        self.playerChoices = {player: None} if player else {}
 
     @classmethod
     def generate_room_id(cls):
         """Generate a random room ID"""
         id_length = 5
         return ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(id_length))
+
+    def add_player(self, player):
+        self.playerChoices.update({player: None})
+
+    def is_full(self):
+        return True if len(self.playerChoices) >= self.team_size else False
+
+    def is_choices(self):
+        return True if None not in self.playerChoices.values() else False
+
+    def set_choice(self, player, choice):
+        self.playerChoices[player] = choice
+
+    def get_result(self):
+        res = {}
+        prev_player = None
+        for player, symbol in self.playerChoices.iteritems():
+            if not prev_player:
+                prev_player = player
+                prev_symbol = symbol
+            else:
+                if symbol == prev_symbol:
+                    res.update({player: 'dead_heat', prev_player: 'dead_heat'})
+                if symbol == STONE and prev_symbol != PAPER:
+                    res.update({player: 'win', prev_player: 'lose'})
+                else:
+                    res.update({player: 'lose', prev_player: 'win'})
+                if symbol == PAPER and prev_symbol != SCISSORS:
+                    res.update({player: 'win', prev_player: 'lose'})
+                else:
+                    res.update({player: 'lose', prev_player: 'win'})
+                if symbol == SCISSORS and prev_symbol != STONE:
+                    res.update({player: 'win', prev_player: 'lose'})
+                else:
+                    res.update({player: 'lose', prev_player: 'win'})
+        return res
--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, emit, send
 import os
-import game
+from game import Game
+
+from collections import deque
 
 # initialize Flask
 app = Flask(__name__)
 socketio = SocketIO(app)
 ROOMS = {}  # dict to track active rooms
+EMPTY_ROOMS = deque()
+PLAYERS = []
 
 
 @app.route('/')
@@ -18,13 +22,39 @@ def index():
 @socketio.on('create')
 def on_create(data):
     """Create a game lobby"""
-    gm = game.Info(
-        team_size=data['size']
-    )
-    room = gm.game_id
-    ROOMS[room] = gm
-    join_room(room)
-    emit('join_room', {'room': room})
+    if len(EMPTY_ROOMS):
+        room_id = EMPTY_ROOMS.popleft()
+        gm = ROOMS.get(room_id)
+        gm.add_player(request.sid)
+    else:
+        gm = Game(
+            team_size=data['size'],
+            player=request.sid
+        )
+        room_id = gm.game_id
+        EMPTY_ROOMS.append(room_id)
+        ROOMS[room_id] = gm
+    join_room(room_id)
+    print 'The game has created. Waiting opponents.'
+    while not gm.is_full():
+        pass
+    emit('join_room', {'room': room_id}, room=room_id)
+
+@socketio.on('game_move')
+def battle(data):
+    """Make battle"""
+    room = data['room']
+    choice = data['symbol']
+    gm = ROOMS.get(room)
+    player = request.sid
+    gm.set_choice(player, choice)
+    print gm.playerChoices
+    print 'ROOMS', ROOMS
+    print 'EMPTY_ROOMS', EMPTY_ROOMS
+    while not gm.is_choices():
+        pass
+    results = gm.get_result()
+    emit('game_res', results, room=player)
 
 
 if __name__ == '__main__':
