8dbaf7bf95e3be610d20d24c2311f3eca5bf3329
Tiskovich <tiskovich.v.u@gmail.com>
2020-03-29 14:17:18 +0300

    recreate tables

--- app/models.py
+++ app/models.py
@@ -12,14 +12,15 @@ class Player(db.Model):
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    rounds = db.relationship('Round', secondary='game_round_link')
-    players = db.relationship('Player', secondary='game_player_link')
+    players = db.relationship('Player', secondary='game_player_link', lazy='subquery',
+        backref=db.backref('games', lazy=True))
+    rounds = db.relationship('Round', backref='game', lazy=True)
 
 
 class Round(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     round = db.Column(db.Integer)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
 
 
 class Result(db.Model):
@@ -29,11 +30,18 @@ class Result(db.Model):
     result = db.Column(db.Integer)
 
 
-class GameRoundLink(db.Model):
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
-    round_id = db.Column(db.Integer, db.ForeignKey('round.id'), primary_key=True)
+# class GameRoundLink(db.Model):
+#     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
+#     round_id = db.Column(db.Integer, db.ForeignKey('round.id'), primary_key=True)
 
 
-class GamePlayerLink(db.Model):
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True)
+game_player_link = db.Table('game_player_link',
+                            db.Column('game_id', db.Integer, db.ForeignKey('game.id'), primary_key=True),
+                            db.Column('player_id', db.Integer, db.ForeignKey('player.id'), primary_key=True)
+                           )
+
+
+
+# class GamePlayerLink(db.Model):
+#     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
+#     player_id = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True)
